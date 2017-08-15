510179c86def1f2136b669e2e3be4516d264c50a
fabuhelow <yousef.wadi@arabiaweather.com>
2017-08-15 13:22:00 +0300

    new theme + fk1 for relational tables

--- src/views/header.blade.php
+++ src/views/header.blade.php
@@ -2,87 +2,191 @@
 <header class="main-header">
 
     <!-- Logo -->
-    <a href="{{url(config('crudbooster.ADMIN_PATH'))}}" title='{{CRUDBooster::getSetting('appname')}}' class="logo">{{CRUDBooster::getSetting('appname')}}</a>
+    {{--<a href="{{url(config('crudbooster.ADMIN_PATH'))}}" title='{{CRUDBooster::getSetting('appname')}}' class="logo">{{CRUDBooster::getSetting('appname')}}</a>--}}
+
+    <!-- Logo -->
+    <a href="{{url(config('crudbooster.ADMIN_PATH'))}}" title='{{CRUDBooster::getSetting('appname')}}' class="logo">
+        <!-- mini logo for sidebar mini 50x50 pixels -->
+        <span class="logo-mini"><b>B</b>PR</span>
+        <!-- logo for regular state and mobile devices -->
+        <span class="logo-lg"><b>Bilforon</b>Portal</span>
+    </a>
 
     <!-- Header Navbar -->
     <nav class="navbar navbar-static-top" role="navigation">
         <!-- Sidebar toggle button-->
-        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
             <span class="sr-only">Toggle navigation</span>
         </a>
+
+
         <!-- Navbar Right Menu -->
         <div class="navbar-custom-menu">
-            <ul class="nav navbar-nav">      
-
-            <li class="dropdown notifications-menu">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" title='Notifications' aria-expanded="false">
-                  <i id='icon_notification' class="fa fa-bell-o"></i>
-                  <span id='notification_count' class="label label-danger" style="display:none">0</span>
-                </a>
-                <ul id='list_notifications' class="dropdown-menu">
-                  <li class="header">{{trans("crudbooster.text_no_notification")}}</li>
-                  <li>
-                    <!-- inner menu: contains the actual data -->
-                    <div class="slimScrollDiv" style="position: relative; overflow: hidden; width: auto; height: 200px;">
-                    <ul class="menu" style="overflow: hidden; width: 100%; height: 200px;">
-                      <li>
-                        <a href="#">
-                          <em>{{trans("crudbooster.text_no_notification")}}</em>
-                        </a>
-                      </li>
-
+            <ul class="nav navbar-nav">
+                <!-- Messages: style can be found in dropdown.less-->
+                <li class="dropdown messages-menu">
+                    <!-- Menu toggle button -->
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <i class="fa fa-envelope-o"></i>
+                        <span class="label label-success">0</span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="header">{{trans("crudbooster.text_no_notification")}}</li>
+                        <li>
+                            <!-- inner menu: contains the messages -->
+                            <ul class="menu">
+                                <li>
+                                    <!-- start message -->
+                                    <a href="#">
+                                        <div class="pull-left">
+                                            <!-- User Image -->
+                                            <img src="{{ CRUDBooster::myPhoto() }}" class="img-circle" alt="User Image">
+                                        </div>
+                                        <!-- Message title and timestamp -->
+                                        <h4>
+                                            {{ CRUDBooster::myName() }}
+                                            <small><i class="fa fa-clock-o"></i> <?php echo date('d F Y')?></small>
+                                        </h4>
+                                        <!-- The message -->
+                                        <p>Why not buy a new awesome theme?</p>
+                                    </a>
+                                </li>
+                                <!-- end message -->
+                            </ul>
+                            <!-- /.menu -->
+                        </li>
+                        <li class="footer"><a href="#">See All Messages</a></li>
                     </ul>
-                    <div class="slimScrollBar" style="width: 3px; position: absolute; top: 0px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 195.122px; background: rgb(0, 0, 0);"></div><div class="slimScrollRail" style="width: 3px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; opacity: 0.2; z-index: 90; right: 1px; background: rgb(51, 51, 51);"></div></div>
-                  </li>
-                  <li class="footer"><a href="{{route('NotificationsControllerGetIndex')}}?m=0">{{trans("crudbooster.text_view_all_notification")}}</a></li>
-                </ul>
-              </li>
+                </li>
+                <!-- /.messages-menu -->
 
+                <!-- Notifications Menu -->
+                <li class="dropdown notifications-menu">
+                    <!-- Menu toggle button -->
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <i class="fa fa-bell-o"></i>
+                        <span class="label label-warning">10</span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="header">You have 10 notifications</li>
+                        <li>
+                            <!-- Inner Menu: contains the notifications -->
+                            <ul class="menu">
+                                <li>
+                                    <!-- start notification -->
+                                    <a href="#">
+                                        <i class="fa fa-users text-aqua"></i> 5 new members joined today
+                                    </a>
+                                </li>
+                                <!-- end notification -->
+                            </ul>
+                        </li>
+                        <li class="footer"><a href="#">View all</a></li>
+                    </ul>
+                </li>
+                <!-- Tasks Menu -->
+                <li class="dropdown tasks-menu">
+                    <!-- Menu Toggle Button -->
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <i class="fa fa-flag-o"></i>
+                        <span class="label label-danger">9</span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="header">You have 9 tasks</li>
+                        <li>
+                            <!-- Inner menu: contains the tasks -->
+                            <ul class="menu">
+                                <li>
+                                    <!-- Task item -->
+                                    <a href="#">
+                                        <!-- Task title and progress text -->
+                                        <h3>
+                                            Design some buttons
+                                            <small class="pull-right">20%</small>
+                                        </h3>
+                                        <!-- The progress bar -->
+                                        <div class="progress xs">
+                                            <!-- Change the css width attribute to simulate progress -->
+                                            <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
+                                                <span class="sr-only">20% Complete</span>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </li>
+                                <!-- end task item -->
+                            </ul>
+                        </li>
+                        <li class="footer">
+                            <a href="#">View all tasks</a>
+                        </li>
+                    </ul>
+                </li>
                 <!-- User Account Menu -->
                 <li class="dropdown user user-menu">
                     <!-- Menu Toggle Button -->
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <!-- The user image in the navbar-->
-                        <img src="{{ CRUDBooster::myPhoto() }}" class="user-image" alt="User Image"/>
+                        <img src="{{ CRUDBooster::myPhoto() }}" class="user-image" alt="User Image">
                         <!-- hidden-xs hides the username on small devices so only the image appears. -->
                         <span class="hidden-xs">{{ CRUDBooster::myName() }}</span>
                     </a>
                     <ul class="dropdown-menu">
                         <!-- The user image in the menu -->
                         <li class="user-header">
-                            <img src="{{ CRUDBooster::myPhoto() }}" class="img-circle" alt="User Image" />
+                            <img src="{{ CRUDBooster::myPhoto() }}" class="img-circle" alt="User Image">
+
                             <p>
-                                {{ CRUDBooster::myName() }}
-                                <small>{{ CRUDBooster::myPrivilegeName() }}</small>
-                                <small><em><?php echo date('d F Y')?></em> </small>                                
+                                {{ CRUDBooster::myName() }} - {{ CRUDBooster::myPrivilegeName() }}
+                                <small><?php echo date('d F Y')?></small>
                             </p>
                         </li>
-
+                        <!-- Menu Body -->
+                        <li class="user-body">
+                            <div class="row">
+                                <div class="col-xs-4 text-center">
+                                    <a href="#">Followers</a>
+                                </div>
+                                <div class="col-xs-4 text-center">
+                                    <a href="#">Sales</a>
+                                </div>
+                                <div class="col-xs-4 text-center">
+                                    <a href="#">Friends</a>
+                                </div>
+                            </div>
+                            <!-- /.row -->
+                        </li>
                         <!-- Menu Footer-->
                         <li class="user-footer">
-                            <div class="pull-{{ trans('crudbooster.left') }}">
-                                <a href="{{ route('AdminCmsUsersControllerGetProfile') }}?m=0" class="btn btn-default btn-flat"><i class='fa fa-user'></i> {{trans("crudbooster.label_button_profile")}}</a>
+                            <div class="pull-left">
+                                <a href="{{ route('AdminCmsUsersControllerGetProfile') }}?m=0" class="btn btn-default btn-flat">{{trans("crudbooster.label_button_profile")}}</a>
                             </div>
-                            <div class="pull-{{ trans('crudbooster.right') }}">
-                                <a title='Lock Screen' href="{{ route('getLockScreen') }}" class='btn btn-default btn-flat'><i class='fa fa-key'></i></a> 
-                                <a href="javascript:void(0)" onclick="swal({   
-                                    title: '{{trans('crudbooster.alert_want_to_logout')}}',                                       
-                                    type:'info',   
-                                    showCancelButton:true, 
-                                    allowOutsideClick:true,  
-                                    confirmButtonColor: '#DD6B55',   
-                                    confirmButtonText: '{{trans('crudbooster.button_logout')}}',   
-                                    cancelButtonText: '{{trans('crudbooster.button_cancel')}}',
-                                    closeOnConfirm: false 
-                                    }, function(){                                                                                 
+                            <div class="pull-right">
+                                <a href="javascript:void(0)" onclick="swal({
+                                        title: '{{trans('crudbooster.alert_want_to_logout')}}',
+                                        type:'info',
+                                        showCancelButton:true,
+                                        allowOutsideClick:true,
+                                        confirmButtonColor: '#DD6B55',
+                                        confirmButtonText: '{{trans('crudbooster.button_logout')}}',
+                                        cancelButtonText: '{{trans('crudbooster.button_cancel')}}',
+                                        closeOnConfirm: false
+                                        }, function(){
                                         location.href = '{{ route("getLogout") }}';
 
-                                    });" title="{{trans('crudbooster.button_logout')}}" class="btn btn-danger btn-flat"><i class='fa fa-power-off'></i></a>
+                                        });" title="{{trans('crudbooster.button_logout')}}" class="btn btn-default btn-flat">Sign out</a>
                             </div>
                         </li>
                     </ul>
                 </li>
+                <!-- Control Sidebar Toggle Button -->
+                <li>
+                    <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+                </li>
             </ul>
         </div>
+
+
+        <!-- Navbar Right Menu zzz-->
+
     </nav>
 </header>
